e9af5a186f8cf54122e7933f4a98d2a884af8e43
Thanh Nam <thanhnam5232@gmail.com>
2021-11-08 12:53:47 +0700

    Tao NavBar

---
+++ WebSUDO/WebSUDO/Models/DangKy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSUDO.Models
+{
+    public class DangKy
+    {
+        [Display(Name = "Email: ")]
+        public string Email { get; set; }
+
+        [Display(Name = "Mật khẩu: ")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "XAc nhận mật khẩu: ")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Mật khẩu và xác nhận mật khẩu phải giống nhau")]
+        public string ConfirmPassword { get; set; }
+    }
+}
