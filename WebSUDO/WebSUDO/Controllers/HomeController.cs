0e73a5513af79cfb542b0af9c4d84d5aec8a5840
Linh Anything <huynhquanglinhpy2016@gmail.com>
2021-11-21 15:40:11 +0700

    Hoàn thành nav

--- WebSUDO/WebSUDO/Controllers/HomeController.cs
+++ WebSUDO/WebSUDO/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace WebSUDO.Controllers
             return View();
         }
 
+        public IActionResult ThongTinTruyen()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
